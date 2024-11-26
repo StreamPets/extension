01a766a885ba22baba64400cd9280482e1dbef42
louisheal <louisheal@live.co.uk>
2024-11-26 14:49:44 +0000

    display the overlay url

--- src/views/Config/Config.tsx
+++ src/views/Config/Config.tsx
@@ -1,10 +1,11 @@
 import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../../contexts/AuthContext";
 import { getOverlayUrl } from "../../api";
+import { AuthContext } from "../../contexts/AuthContext";
+import "./Config.css";
 
 const Config = () => {
 	const { token } = useContext(AuthContext);
-	const [overlayUrl, setOverlayUrl] = useState(null);
+	const [overlayUrl, setOverlayUrl] = useState<string | undefined>();
 
 	useEffect(() => {
 		if (!token) {
@@ -19,7 +20,18 @@ const Config = () => {
 		fetchOverlayUrl();
 	}, [token]);
 
-	return <p>{overlayUrl}</p>;
+	return (
+		<div className="container">
+			<div className="overlay-url">{overlayUrl}</div>
+			<button
+				className="launch-button"
+				type="button"
+				onClick={() => window.open(overlayUrl, "_blank")}
+			>
+				Launch
+			</button>
+		</div>
+	);
 };
 
 export default Config;
